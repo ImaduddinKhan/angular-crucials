da3fc840671c0e49aab1f03ddd8f17be8836575d
ImadUdDin <emaduddin181@gmail.com>
2022-06-20 00:37:33 +0500

    form grouping and overriding form data

--- src/poc-ng-forms/src/app/app.component.ts
+++ src/poc-ng-forms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,34 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm | undefined;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['Male', 'Female'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm?.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   QAnswer: '',
+    //   gender: 'male',
+    // });
+    this.signUpForm?.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  onSubmit() {
+    console.log(this.signUpForm);
   }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
 }
