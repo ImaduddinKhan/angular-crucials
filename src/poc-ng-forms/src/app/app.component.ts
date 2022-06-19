9c3e5fbd1519cc6a4f4af88296bc01912d443835
ImadUdDin <emaduddin181@gmail.com>
2022-06-19 07:34:15 +0500

    ng form (TD approach) skeleton

--- src/poc-ng-forms/src/app/app.component.ts
+++ src/poc-ng-forms/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'poc-ng-forms';
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 }
