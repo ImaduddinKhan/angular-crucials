9f5efc915c893bb1bab8a171c233a08f4976c19f
ImadUdDin <emaduddin181@gmail.com>
2022-06-24 17:47:36 +0500

    adding post and get methods

--- src/poc-ng-http/src/app/app.component.ts
+++ src/poc-ng-http/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +12,49 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http
+      .post(
+        'https://ng-restful-guide-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
+        postData
+      )
+      .subscribe((resposeData) => {
+        console.log(resposeData);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(
+        'https://ng-restful-guide-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postArray;
+        })
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+      });
   }
 }
