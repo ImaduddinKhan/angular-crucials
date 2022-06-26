43cd489078f8dab289b0e1ca203cdcb309e8801f
ImadUdDin <emaduddin181@gmail.com>
2022-06-27 02:55:03 +0500

    adding loading spinner

--- src/shopping-list-app/src/app/auth/auth.component.ts
+++ src/shopping-list-app/src/app/auth/auth.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true;
+  isLoading = false;
 
   constructor(private authService: AuthService) {}
   ngOnInit(): void {}
@@ -24,15 +25,18 @@ export class AuthComponent implements OnInit {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     if (this.isLoginMode) {
       //...
     } else {
       this.authService.signUp(email, password).subscribe(
         (responseData) => {
           console.log(responseData);
+          this.isLoading = false;
         },
         (error) => {
           console.log(error.message);
+          this.isLoading = false;
         }
       );
     }
