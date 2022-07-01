ab170e221a4ed27f473d5c4d8640d567b5ebf068
ImadUdDin <emaduddin181@gmail.com>
2022-07-02 01:00:55 +0500

    loading services in different ways (DI)

--- src/shopping-list-app/src/app/app.module.ts
+++ src/shopping-list-app/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -19,7 +20,7 @@ import { CoreModule } from './core.module';
     SharedModule,
     CoreModule,
   ],
-  providers: [],
+  providers: [LoggingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
