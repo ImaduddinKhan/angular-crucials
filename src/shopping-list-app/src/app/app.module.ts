685e98989544cd329656cc8e3b31a7349fe201c0
ImadUdDin <emaduddin181@gmail.com>
2022-07-01 19:03:45 +0500

    adding lazy loading for /recipes

--- src/shopping-list-app/src/app/app.module.ts
+++ src/shopping-list-app/src/app/app.module.ts
@@ -5,8 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RecipesModule } from './recipes/recipes.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -19,7 +18,6 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
