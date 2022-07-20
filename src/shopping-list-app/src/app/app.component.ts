1a761df519933aae7cf721a9a15c3044a5a4f171
ImadUdDin <emaduddin181@gmail.com>
2022-07-21 01:42:54 +0500

    adding nestJs/ng-universal

--- src/shopping-list-app/src/app/app.component.ts
+++ src/shopping-list-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { isPlatformBrowser } from '@angular/common';
+
 import { AuthService } from './auth/auth.service';
 import { LoggingService } from './logging.service';
 import * as formApp from './store/app.reducer';
@@ -14,12 +16,15 @@ export class AppComponent implements OnInit {
   constructor(
     private store: Store<formApp.AppState>,
     private authService: AuthService,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    @Inject(PLATFORM_ID) private platformId
   ) {}
 
   ngOnInit(): void {
     // this.authService.autoLogin();
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.loggingService.pringLog('Hello from app component ngOnInit');
   }
 }
